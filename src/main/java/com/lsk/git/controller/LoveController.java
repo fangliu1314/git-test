3cc9fc681bc862c091ec2aac398e5137558864b9
LiuShengkang <1252528791@qq.com>
2022-06-03 08:35:31 +0800

    master aiqing commit

---
+++ src/main/java/com/lsk/git/controller/LoveController.java
@@ -0,0 +1,17 @@
+package com.lsk.git.controller;
+
+/**
+ * @ClassName LoveController.java
+ * @ProjectName: git-test
+ * @Description: TODO
+ * @author: LiuShengkang
+ * @@version 1.0.0
+ * @createTime 2022年06月03日 08:34:00
+ */
+public class LoveController {
+    public static void main(String[] args) {
+
+        System.out.println("我们的爱情");
+    }
+
+}
