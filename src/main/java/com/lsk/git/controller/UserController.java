a79721ef235dab508800964a7ffea952bb7a9eea
LiuShengkang <1252528791@qq.com>
2022-06-03 08:06:34 +0800

    first commit

---
+++ src/main/java/com/lsk/git/controller/UserController.java
@@ -0,0 +1,20 @@
+package com.lsk.git.controller;
+
+/**
+ * @ClassName UserController.java
+ * @ProjectName: git-test
+ * @Description: TODO
+ * @author: LiuShengkang
+ * @@version 1.0.0
+ * @createTime 2022年06月03日 08:01:00
+ */
+public class UserController {
+
+    public static void main(String[] args) {
+
+        System.out.println("初恋 笑容");
+
+    }
+
+
+}
